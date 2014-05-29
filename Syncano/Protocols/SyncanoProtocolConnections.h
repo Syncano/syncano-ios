2406d2cf231642aa7efc4d71becbcc864306b241
Mariusz Wisniewski <mariusz.wisniewski@mindpower.pl>
2014-05-29 18:24:24 +0200

    Refactoring

--- Syncano/Protocols/SyncanoProtocolConnections.h
+++ Syncano/Protocols/SyncanoProtocolConnections.h
@@ -10,10 +10,10 @@
 #import "SyncanoResponse_Connections.h"
 
 /**
- SyncanoProtocolConnections is used to transmit information about SyncanoConnection objects
- 
- @note All `SyncanoProtocol*` protocols define method signatures used by objects (i.e. Syncano, SyncanoSyncServer communicating with Syncano API.
-  @note This protocol defines only asynchronous methods, it is to be used only with SyncanoSyncServer
+   SyncanoProtocolConnections is used to transmit information about SyncanoConnection objects
+
+   @note All `SyncanoProtocol*` protocols define method signatures used by objects (i.e. Syncano, SyncanoSyncServer communicating with Syncano API.
+   @note This protocol defines only asynchronous methods, it is to be used only with SyncanoSyncServer
  */
 @protocol SyncanoProtocolConnections <NSObject>
 @required
@@ -29,21 +29,21 @@
 ///-
 
 /**
- Get connection list
- 
- @param params Connection list parameters
+   Get currently connected API client connections up to a limit (max 100).
+
+   @param params Connection list parameters
  */
 - (void)connectionGet:(SyncanoParameters_Connections_Get *)params callback:(void (^)(SyncanoResponse_Connections_Get *response))callback;
 /**
- Get all connections list
- 
- @param params connection list parameters
+   Get all connections from current instance up to a limit (max 100).
+
+   @param params Connection list parameters
  */
 - (void)connectionGetAll:(SyncanoParameters_Connections_Get_All *)params callback:(void (^)(SyncanoResponse_Connections_Get_All *response))callback;
 /**
- Update existing connection
- 
- @param params Update connection parameters
+   Updates specified API client connection info.
+
+   @param params Update connection parameters
  */
 - (void)connectionUpdate:(SyncanoParameters_Connections_Update *)params callback:(void (^)(SyncanoResponse_Connections_Update *response))callback;
 
