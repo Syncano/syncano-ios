3d9fbdd224bf64d2f3c914d9632af63707a4f0b3
Mariusz Wisniewski <mariusz.wisniewski@mindpower.pl>
2014-05-29 18:16:42 +0200

    Added new method parameters, updated docs

--- Syncano/Parameters/Users/SyncanoParameters_Users.h
+++ Syncano/Parameters/Users/SyncanoParameters_Users.h
@@ -9,151 +9,208 @@
 #import "SyncanoParameters.h"
 
 /**
- Common user parameters
+   Common user parameters
  */
 @interface SyncanoParameters_Users : SyncanoParameters
 /**
- User ID
+   User ID. Optional for User API key with associated user, otherwise it's mandatory.
  */
 @property (strong)    NSString *userId;
 /**
- User name
+   User name. Optional for User API key with associated user, otherwise it's mandatory.
  */
 @property (strong)    NSString *userName;
 /**
- @return SyncanoParameters object with required fields initialized
+   @return SyncanoParameters object with required fields initialized
  */
 - (SyncanoParameters_Users *)initWithUserName:(NSString *)userName;
 /**
- @return SyncanoParameters object with required fields initialized
+   @return SyncanoParameters object with required fields initialized
  */
 - (SyncanoParameters_Users *)initWithUserId:(NSString *)userId;
+/**
+   @return SyncanoParameters
+ */
+- (SyncanoParameters_Users *)init;
 
 @end
 
 /**
- Creates a new user.
+   Logs in a user.
+ */
+@interface SyncanoParameters_Users_Login : SyncanoParameters
+/**
+   User name.
+ */
+@property (strong) NSString *userName;
+/**
+   User's password.
+ */
+@property (strong) NSString *password;
+
+- (SyncanoParameters_Users_Login *)initWithUserName:(NSString *)userName password:(NSString *)password;
+
+@end
+
+/**
+   Creates a new user.
  */
 @interface SyncanoParameters_Users_New : SyncanoParameters
 /**
- User name.
+   User name.
  */
-@property (strong)    NSString *userName;
+@property (strong) NSString *userName;
 /**
- User's nick.
+   User's nick.
  */
-@property (strong)    NSString *nick;
+@property (strong) NSString *nick;
 /**
- User's avatar.
+   User's avatar.
  */
-@property (strong)    UIImage *avatar;
+@property (strong) UIImage *avatar;
 /**
- @return SyncanoParameters object with required fields initialized
+   User's password.
  */
-- (SyncanoParameters_Users_New*)initWithUserName:(NSString*)userName;
+@property (strong) NSString *password;
+/**
+   @return SyncanoParameters object with required fields initialized
+ */
+- (SyncanoParameters_Users_New *)initWithUserName:(NSString *)userName;
+/**
+   @return SyncanoParameters object with required fields initialized
+ */
+- (SyncanoParameters_Users_New *)initWithUserName:(NSString *)userName password:(NSString *)password;
 
 @end
 
 /**
- Gets all users from within instance.
- 
- @note To paginate and to get more data, use since_id or since_time parameter.
+   Gets all users from within instance.
+
+   @note To paginate and to get more data, use since_id or since_time parameter.
  */
 @interface SyncanoParameters_Users_GetAll : SyncanoParameters
 /**
- If specified, will only return users with id higher than since_id (newer).
+   If specified, will only return users with id higher than since_id (newer).
  */
 @property (strong)    NSString *sinceId;
 /**
- Number of users to be returned. Default and max value: 100
+   Number of users to be returned. Default and max value: 100
  */
 @property (strong)    NSNumber *limit;
 
 @end
 
 /**
- Gets users of specified criteria that are associated with Data Objects within specified collection.
+   Gets users of specified criteria that are associated with Data Objects within specified collection.
  */
 @interface SyncanoParameters_Users_Get : SyncanoParameters_ProjectId_CollectionId_CollectionKey
 
 /**
- Returns only users whose Data Objects are in specified state. Possible values: Pending, Moderated, All. Default value: All.
+   Returns only users whose Data Objects are in specified state. Possible values: Pending, Moderated, All. Default value: All.
  */
 @property (strong)    NSString *state;
 /**
- Folder name that data will be returned from. Max 100 values per request. If not present returns data from across all collection folders.
+   Folder name that data will be returned from. Max 100 values per request. If not present returns data from across all collection folders.
  */
 @property (strong)    NSArray *folders;
 /**
- Filtering by related Data Object's content. Possible values:
- 
- TEXT - only return users that sent data with text,
- IMAGE - only return users that sent data with an image.
+   Filtering by related Data Object's content. Possible values:
+
+   TEXT - only return users that sent data with text,
+   IMAGE - only return users that sent data with an image.
  */
 @property (strong, nonatomic)    NSString *filter;
 
 @end
 
 /**
- Get one user.
+   Get one user.
  */
 @interface SyncanoParameters_Users_GetOne : SyncanoParameters_Users
 
 @end
 
 /**
- Updates a specified user.
+   Updates a specified user.
  */
 @interface SyncanoParameters_Users_Update : SyncanoParameters_Users
+
 /**
- New user nick.
+   User id defining user. If both id and name are specified, will use id for getting user while user_name will be updated with provided new value. userId is automatically filled when used with User API key.
+ */
+@property (strong) NSString *userId;
+/**
+   User name defining user. If both id and name are specified, will use id for getting user while user_name will be updated with provided new value. User_id is automatically filled when used with User API key.
+ */
+@property (strong) NSString *userName;
+/**
+   New user nick.
  */
 @property (strong)  NSString *nick;
 /**
- User avatar. If specified as empty string - will instead delete current avatar.
+   User avatar. If specified as empty string - will instead delete current avatar.
  */
 @property (strong)  UIImage *avatar;
+/**
+   New user password.
+ */
+@property (strong) NSString *passwordNew;
+/**
+   Current password for confirmation. Required only when used with User API key.
+ */
+@property (strong) NSString *passwordCurrent;
+
+/**
+   @param currentPassword Current password for confirmation. Required only when used with User API key.
+
+   @return SyncanoParameters object with required fields initialized
+ */
+- (SyncanoParameters_Users_Update *)initWithCurrentPassword:(NSString *)currentPassword;
+/**
+   @return SyncanoParameters object with required fields initialized
+ */
+- (SyncanoParameters_Users_Update *)init;
 
 @end
 
 /**
- Count users of specified criteria.
+   Count users of specified criteria.
  */
 @interface SyncanoParameters_Users_Count : SyncanoParameters
 
 /**
- Project id (optional). If defined, will only count users that has a Data Object associated within project.
+   Project id (optional). If defined, will only count users that has a Data Object associated within project.
  */
 @property (strong)    NSString *project_id;
 /**
- Collection id or key defining collection (optional). If defined, will only count users that has a Data Object associated within collection.
+   Collection id defining collection (optional). If defined, will only count users that has a Data Object associated within collection.
  */
 @property (strong)    NSString *collection_id;
 /**
- Collection id or key defining collection (optional). If defined, will only count users that has a Data Object associated within collection.
+   Collection key defining collection (optional). If defined, will only count users that has a Data Object associated within collection.
  */
 @property (strong)    NSString *collection_key;
 /**
- Return only users whose Data Objects are in specified state. Accepted values: Pending, Moderated, All. Default value: All.
+   Return only users whose Data Objects are in specified state. Accepted values: Pending, Moderated, All. Default value: All.
  */
 @property (strong)    NSString *state;
 
 /**
- Folder name that data will be returned from. Max 100 values per request. If not present returns data from across all collection folders.
+   Folder name that data will be returned from. Max 100 values per request. If not present returns data from across all collection folders.
  */
 @property (strong)    NSArray *folders;
 /**
- Filtering by related Data Object's content. Possible values:
- 
- TEXT - only count users that sent data with text,
- IMAGE - only count users that sent data with an image.
+   Filtering by related Data Object's content. Possible values:
+
+   TEXT - only count users that sent data with text,
+   IMAGE - only count users that sent data with an image.
  */
 @property (strong, nonatomic)    NSString *filter;
 
 @end
 
 /**
- Deletes (permanently) specified user and all associated data.
+   Deletes (permanently) specified user and all associated data.
  */
 @interface SyncanoParameters_Users_Delete : SyncanoParameters_Users
 
--- Syncano/Parameters/Users/SyncanoParameters_Users.m
+++ Syncano/Parameters/Users/SyncanoParameters_Users.m
@@ -13,31 +13,74 @@
 @implementation SyncanoParameters_Users : SyncanoParameters
 
 - (SyncanoParameters_Users *)initWithUserName:(NSString *)userName {
-    self = [super init];
-    if (self) {
-        self.userName = userName;
-        [self validateSpecialParameters:@[@"userName"]];
-    }
-    return self;
+	self = [super init];
+	if (self) {
+		self.userName = userName;
+		[self validateSpecialParameters:@[@"userName"]];
+	}
+	return self;
 }
 
 - (SyncanoParameters_Users *)initWithUserId:(NSString *)userId {
-    self = [super init];
-    if (self) {
-        self.userId = userId;
-        [self validateSpecialParameters:@[@"userId"]];
-    }
-    return self;
+	self = [super init];
+	if (self) {
+		self.userId = userId;
+		[self validateSpecialParameters:@[@"userId"]];
+	}
+	return self;
 }
 
-- (NSArray *)initalizeSelectorNamesArray {
-    return @[@"initWithUserName:", @"initWithUserId:"];
+- (SyncanoParameters_Users *)init {
+	self = [super init];
+	if (self) {
+	}
+	return self;
+}
+
+- (NSArray *)initializeSelectorNamesArray {
+	return @[@"initWithUserName:", @"initWithUserId:", @"init"];
 }
 
 + (NSDictionary *)JSONKeyPathsByPropertyKey {
-    NSDictionary * parameters = @{@"userName" : @"user_name",
-                                  @"userId" : @"user_id"};
-    return [SyncanoParameters mergeSuperParameters:[super JSONKeyPathsByPropertyKey] parameters:parameters];
+	NSDictionary *parameters = @{ @"userName" : @"user_name",
+		                          @"userId" : @"user_id" };
+	return [SyncanoParameters mergeSuperParameters:[super JSONKeyPathsByPropertyKey] parameters:parameters];
+}
+
+@end
+
+@implementation SyncanoParameters_Users_Login : SyncanoParameters
+
+- (SyncanoParameters_Users_Login *)initWithUserName:(NSString *)userName password:(NSString *)password {
+	self = [super init];
+	if (self) {
+		self.userName = userName;
+		self.password = password;
+		[self validateParameters];
+	}
+	return self;
+}
+
+- (NSArray *)requiredParametersNames {
+	return @[@"userName", @"password"];
+}
+
+- (SEL)initalizeSelector {
+	return @selector(initWithUserName:password:);
+}
+
+- (NSString *)methodName {
+	return @"user.login";
+}
+
+- (id)responseFromJSON:(NSDictionary *)json {
+	return [SyncanoResponse_Users_Login responseFromJSON:json];
+}
+
++ (NSDictionary *)JSONKeyPathsByPropertyKey {
+	NSDictionary *parameters = @{ @"userName" : @"user_name",
+		                          @"password" : @"password" };
+	return [SyncanoParameters mergeSuperParameters:[super JSONKeyPathsByPropertyKey] parameters:parameters];
 }
 
 @end
@@ -45,20 +88,31 @@
 @implementation SyncanoParameters_Users_New : SyncanoParameters
 
 - (SyncanoParameters_Users_New *)initWithUserName:(NSString *)userName {
-    self = [super init];
-    if (self) {
-        self.userName = userName;
-        [self validateParameters];
-    }
-    return self;
+	self = [super init];
+	if (self) {
+		self.userName = userName;
+		[self validateParameters];
+	}
+	return self;
 }
 
-- (SEL)initalizeSelector {
-    return @selector(initWithUserName:);
+- (SyncanoParameters_Users_New *)initWithUserName:(NSString *)userName password:(NSString *)password {
+	self = [super init];
+	if (self) {
+		self.userName = userName;
+		self.password = password;
+		[self validateSpecialParameters:@[@"userName", @"password"]];
+	}
+	return self;
 }
 
-- (NSArray*)requiredParametersNames {
-    return @[@"userName"];
+- (NSArray *)initializeSelectorNamesArray {
+	return @[@"initWithUserName:",
+	         @"initWithUserName:password:"];
+}
+
+- (NSArray *)requiredParametersNames {
+	return @[@"userName"];
 }
 
 - (NSString *)methodName {
@@ -70,10 +124,11 @@
 }
 
 + (NSDictionary *)JSONKeyPathsByPropertyKey {
-    NSDictionary * parameters = @{@"userName" : @"user_name",
-                                  @"nick" : @"nick",
-                                  @"avatar" : @"avatar"};
-    return [SyncanoParameters mergeSuperParameters:[super JSONKeyPathsByPropertyKey] parameters:parameters];
+	NSDictionary *parameters = @{ @"userName" : @"user_name",
+		                          @"nick" : @"nick",
+		                          @"avatar" : @"avatar",
+		                          @"password" : @"password" };
+	return [SyncanoParameters mergeSuperParameters:[super JSONKeyPathsByPropertyKey] parameters:parameters];
 }
 
 @end
@@ -89,9 +144,9 @@
 }
 
 + (NSDictionary *)JSONKeyPathsByPropertyKey {
-    NSDictionary * parameters = @{@"sinceId" : @"since_id",
-                                  @"folders" : @"folder"};
-    return [SyncanoParameters mergeSuperParameters:[super JSONKeyPathsByPropertyKey] parameters:parameters];
+	NSDictionary *parameters = @{ @"sinceId" : @"since_id",
+		                          @"folders" : @"folder" };
+	return [SyncanoParameters mergeSuperParameters:[super JSONKeyPathsByPropertyKey] parameters:parameters];
 }
 
 @end
@@ -107,23 +162,24 @@
 }
 
 + (NSDictionary *)JSONKeyPathsByPropertyKey {
-    NSDictionary * parameters = @{@"state" : @"state",
-                                  @"folders" : @"folder",
-                                  @"filter" : @"filter"};
-    return [SyncanoParameters mergeSuperParameters:[super JSONKeyPathsByPropertyKey] parameters:parameters];
+	NSDictionary *parameters = @{ @"state" : @"state",
+		                          @"folders" : @"folder",
+		                          @"filter" : @"filter" };
+	return [SyncanoParameters mergeSuperParameters:[super JSONKeyPathsByPropertyKey] parameters:parameters];
 }
 
 - (void)setFilter:(NSString *)filter {
-    NSArray * acceptedValues = [self acceptedFilterValues];
-    if ([acceptedValues containsObject:[filter lowercaseString]]) {
-        _filter = filter;
-    } else {
-        [NSException raise:@"Wrong parameter value" format:@"Allowed values for filter parameter: %@ (use kSyncanoParametersFilter constans)", acceptedValues];
-    }
+	NSArray *acceptedValues = [self acceptedFilterValues];
+	if ([acceptedValues containsObject:[filter lowercaseString]]) {
+		_filter = filter;
+	}
+	else {
+		[NSException raise:@"Wrong parameter value" format:@"Allowed values for filter parameter: %@ (use kSyncanoParametersFilter constans)", acceptedValues];
+	}
 }
 
-- (NSArray*)acceptedFilterValues {
-    return @[kSyncanoParametersFilterImage,kSyncanoParametersFilterText];
+- (NSArray *)acceptedFilterValues {
+	return @[kSyncanoParametersFilterImage, kSyncanoParametersFilterText];
 }
 
 @end
@@ -142,6 +198,28 @@
 
 @implementation SyncanoParameters_Users_Update
 
+- (SyncanoParameters_Users_Update *)initWithCurrentPassword:(NSString *)currentPassword {
+	self = [super init];
+	if (self) {
+		self.passwordCurrent = currentPassword;
+		[self validateSpecialParameters:@[@"passwordCurrent"]];
+	}
+	return self;
+}
+
+- (SyncanoParameters_Users_Update *)init {
+	self = [super init];
+	if (self) {
+	}
+	return self;
+}
+
+- (NSArray *)initializeSelectorNamesArray {
+	NSArray *ownInitializers = @[@"initWithCurrentPassword:", @"init"];
+	NSArray *allInitializers = [ownInitializers arrayByAddingObjectsFromArray:[super initializeSelectorNamesArray]];
+	return allInitializers;
+}
+
 - (NSString *)methodName {
 	return @"user.update";
 }
@@ -151,9 +229,11 @@
 }
 
 + (NSDictionary *)JSONKeyPathsByPropertyKey {
-    NSDictionary * parameters = @{@"nick" : @"nick",
-                                  @"avatar" : @"avatar"};
-    return [SyncanoParameters mergeSuperParameters:[super JSONKeyPathsByPropertyKey] parameters:parameters];
+	NSDictionary *parameters = @{ @"nick" : @"nick",
+		                          @"avatar" : @"avatar",
+		                          @"passwordNew" : @"new_password",
+		                          @"passwordCurrent" : @"current_password" };
+	return [SyncanoParameters mergeSuperParameters:[super JSONKeyPathsByPropertyKey] parameters:parameters];
 }
 
 @end
@@ -169,28 +249,28 @@
 }
 
 + (NSDictionary *)JSONKeyPathsByPropertyKey {
-    NSDictionary * parameters = @{@"projectId" : @"project_id",
-                                  @"collectionId" : @"collection_id",
-                                  @"collectionKey" : @"collection_key",
-                                  @"state" : @"state",
-                                  @"folders" : @"folders",
-                                  @"filter" : @"filter"};
-    return [SyncanoParameters mergeSuperParameters:[super JSONKeyPathsByPropertyKey] parameters:parameters];
+	NSDictionary *parameters = @{ @"projectId" : @"project_id",
+		                          @"collectionId" : @"collection_id",
+		                          @"collectionKey" : @"collection_key",
+		                          @"state" : @"state",
+		                          @"folders" : @"folders",
+		                          @"filter" : @"filter" };
+	return [SyncanoParameters mergeSuperParameters:[super JSONKeyPathsByPropertyKey] parameters:parameters];
 }
 
 - (void)setFilter:(NSString *)filter {
-    NSArray * acceptedValues = [self acceptedFilterValues];
-    if ([acceptedValues containsObject:[filter lowercaseString]]) {
-        _filter = filter;
-    } else {
-        [NSException raise:@"Wrong parameter value" format:@"Allowed values for filter parameter: %@ (use kSyncanoParametersFilter constans)", acceptedValues];
-    }
+	NSArray *acceptedValues = [self acceptedFilterValues];
+	if ([acceptedValues containsObject:[filter lowercaseString]]) {
+		_filter = filter;
+	}
+	else {
+		[NSException raise:@"Wrong parameter value" format:@"Allowed values for filter parameter: %@ (use kSyncanoParametersFilter constans)", acceptedValues];
+	}
 }
 
-- (NSArray*)acceptedFilterValues {
-    return @[kSyncanoParametersFilterImage,kSyncanoParametersFilterText];
+- (NSArray *)acceptedFilterValues {
+	return @[kSyncanoParametersFilterImage, kSyncanoParametersFilterText];
 }
-
 
 @end
 
