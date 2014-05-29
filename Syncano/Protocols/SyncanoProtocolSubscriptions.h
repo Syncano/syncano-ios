96a60ab17101c02577ffd2802d1a5eafcd1924ff
Mariusz Wisniewski <mariusz.wisniewski@mindpower.pl>
2014-05-29 18:23:29 +0200

    Refactoring, docs updates

--- Syncano/Protocols/SyncanoProtocolSubscriptions.h
+++ Syncano/Protocols/SyncanoProtocolSubscriptions.h
@@ -13,10 +13,10 @@
 #import "SyncanoResponse_Subscriptions.h"
 
 /**
- SyncanoProtocolSubscriptions is used to transmit information about SyncanoSubscription objects
- 
- @note All `SyncanoProtocol*` protocols define method signatures used by objects (i.e. Syncano, SyncanoSyncServer communicating with Syncano API.
- @note This protocol defines only asynchronous methods, it is to be used only with SyncanoSyncServer
+   SyncanoProtocolSubscriptions is used to transmit information about SyncanoSubscription objects
+
+   @note All `SyncanoProtocol*` protocols define method signatures used by objects (i.e. Syncano, SyncanoSyncServer communicating with Syncano API.
+   @note This protocol defines only asynchronous methods, it is to be used only with SyncanoSyncServer
  */
 @protocol SyncanoProtocolSubscriptions <NSObject>
 @required
@@ -32,33 +32,43 @@
 ///-
 
 /**
- Subscribe to project.
- 
- @param params   parameters for subscription
+   Subscribe to project.
+
+   @note User API key usage permitted with context session or connection if subscribe permission is added through apikey.authorize() and read_data permission is added to specified project through project.authorize().
+
+   @param params   parameters for subscription
  */
 - (void)subscriptionSubscribeProject:(SyncanoParameters_Subscriptions_SubscribeProject *)params callback:(void (^)(SyncanoResponse *response))callback;
 /**
- Unsubscribe project.
- 
- @param params   parameters for unsubscription
+   Unsubscribe project.
+
+   @note User API key usage permitted.
+
+   @param params   parameters for unsubscription
  */
 - (void)subscriptionUnsubscribeProject:(SyncanoParameters_Subscriptions_UnsubscribeProject *)params callback:(void (^)(SyncanoResponse *response))callback;
 /**
- Subscribe to collection within specified project.
- 
- @param params   parameters for subscription
+   Subscribe to collection within specified project.
+
+   @note User API key usage permitted with context session or connection if subscribe permission is added through apikey.authorize() and read_data permission is added to specified collection through collection.authorize() or project.authorize().
+
+   @param params   parameters for subscription
  */
 - (void)subscriptionSubscribeCollection:(SyncanoParameters_Subscriptions_SubscribeCollection *)params callback:(void (^)(SyncanoResponse *response))callback;
 /**
- Unsubscribe to collection within specified project.
- 
- @param params   parameters for unsubscription
+   Unsubscribe to collection within specified project.
+
+   @note User API key usage permitted.
+
+   @param params   parameters for unsubscription
  */
 - (void)subscriptionUnsubscribeCollection:(SyncanoParameters_Subscriptions_UnsubscribeCollection *)params callback:(void (^)(SyncanoResponse *response))callback;
 /**
- Get subscriptions
- 
- @param params   Parameters for getting subscriptions
+   Get subscriptions
+
+   @note User API key usage permitted.
+
+   @param params   Parameters for getting subscriptions
  */
 - (void)subscriptionGet:(SyncanoParameters_Subscriptions_Get *)params callback:(void (^)(SyncanoResponse_Subscriptions_Get *response))callback;
 
