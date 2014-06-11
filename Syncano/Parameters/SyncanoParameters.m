35833054547b7b092943b99c7a74f493b6d20359
Jaroslaw Gliwinski <jaroslaw.gliwinski@appunite.com>
2014-06-11 14:19:36 +0200

    Revert "Temporary fix for checking used selector"
    This reverts commit 671a41660c40bd15f3c9a767aa772fc1ad0f43b6.

--- Syncano/Parameters/SyncanoParameters.m
+++ Syncano/Parameters/SyncanoParameters.m
@@ -52,32 +52,30 @@ NSString *const kSyncanoParametersFilterImage = @"image";
 	if (self) {
 		_timezone = @"UTC";
 
-		/*
-		   NSString *sourceString = [[NSThread callStackSymbols] objectAtIndex:1];
-		   NSCharacterSet *separatorSet = [NSCharacterSet characterSetWithCharactersInString:@" -[]+?.,"];
-		   NSMutableArray *array = [NSMutableArray arrayWithArray:[sourceString componentsSeparatedByCharactersInSet:separatorSet]];
-		   [array removeObject:@""];
+		NSString *sourceString = [[NSThread callStackSymbols] objectAtIndex:1];
+		NSCharacterSet *separatorSet = [NSCharacterSet characterSetWithCharactersInString:@" -[]+?.,"];
+		NSMutableArray *array = [NSMutableArray arrayWithArray:[sourceString componentsSeparatedByCharactersInSet:separatorSet]];
+		[array removeObject:@""];
 
-		   NSString *selectorString = [array objectAtIndex:4];
-		   NSArray *initalizeSelectorNames = [self initializeSelectorNamesArray];
-		   SEL initalizeSelector = NSSelectorFromString(selectorString);
+		NSString *selectorString = [array objectAtIndex:4];
+		NSArray *initalizeSelectorNames = [self initializeSelectorNamesArray];
+		SEL initalizeSelector = NSSelectorFromString(selectorString);
 
-		   if (self.initalizeSelector) {
-		    if (initalizeSelector != self.initalizeSelector) {
-		        [NSException raise:@"Invaild initalization" format:@"Use: %@ for initalization", NSStringFromSelector(self.initalizeSelector)];
-		    }
+		if (self.initalizeSelector) {
+			if (initalizeSelector != self.initalizeSelector) {
+				[NSException raise:@"Invaild initalization" format:@"Use: %@ for initalization", NSStringFromSelector(self.initalizeSelector)];
+			}
 
-		    if (!self.requiredParametersNames) {
-		        [NSException raise:@"Bad implementation" format:@"%@ has unique init method (%@) and 0 required parameters, implemement requiredParametersNames method", NSStringFromClass(self.class), NSStringFromSelector(self.initalizeSelector)];
-		    }
-		   }
-		   else if (initalizeSelectorNames && [initalizeSelectorNames count] > 0) {
-		    if (![initalizeSelectorNames containsObject:selectorString]) {
-		        NSString *allowedSelectors = [initalizeSelectorNames componentsJoinedByString:@" or "];
-		        [NSException raise:@"Invaild initalization" format:@"Use: %@ for initalization %@ class", allowedSelectors, NSStringFromClass(self.class)];
-		    }
-		   }
-		 */
+			if (!self.requiredParametersNames) {
+				[NSException raise:@"Bad implementation" format:@"%@ has unique init method (%@) and 0 required parameters, implemement requiredParametersNames method", NSStringFromClass(self.class), NSStringFromSelector(self.initalizeSelector)];
+			}
+		}
+		else if (initalizeSelectorNames && [initalizeSelectorNames count] > 0) {
+			if (![initalizeSelectorNames containsObject:selectorString]) {
+				NSString *allowedSelectors = [initalizeSelectorNames componentsJoinedByString:@" or "];
+				[NSException raise:@"Invaild initalization" format:@"Use: %@ for initalization %@ class", allowedSelectors, NSStringFromClass(self.class)];
+			}
+		}
 	}
 	return self;
 }
