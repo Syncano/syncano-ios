f80828f40a29700b40fa49e8ccdf6cfb67d9f615
Jan Lipmann <jan.lipmann@likomp.pl>
2015-10-08 11:18:34 +0200

    - SCOfflineStore class added - SCSQLiteDatabaseResult class added

---
+++ syncano-ios/SCOfflineStore.m
@@ -0,0 +1,13 @@
+//
+//  SCOfflineStore.m
+//  syncano-ios
+//
+//  Created by Jan Lipmann on 08/10/15.
+//  Copyright © 2015 Syncano. All rights reserved.
+//
+
+#import "SCOfflineStore.h"
+
+@implementation SCOfflineStore
+
+@end
---
+++ syncano-ios/SCSQLiteDatabaseResult.m
@@ -0,0 +1,13 @@
+//
+//  SCSQLiteDatabaseResult.m
+//  syncano-ios
+//
+//  Created by Jan Lipmann on 08/10/15.
+//  Copyright © 2015 Syncano. All rights reserved.
+//
+
+#import "SCSQLiteDatabaseResult.h"
+
+@implementation SCSQLiteDatabaseResult
+
+@end
