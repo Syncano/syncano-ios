208a9c2919e5ef8a5009a8f5c45ed36d8ebe1a74
Jan Lipmann <jan.lipmann@likomp.pl>
2015-10-28 22:51:07 +0100

    minor fixes

--- syncano-ios/Syncano.h
+++ syncano-ios/Syncano.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2015 Syncano. All rights reserved.
 //
 
-
-
 #import <Foundation/Foundation.h>
 #import "SCMacros.h"
 #import "SCConstants.h"
@@ -24,14 +22,6 @@
 #import "SCWebhook.h"
 #import "SCChannel.h"
 #import "SCFile.h"
-
-//! Project version number for Syncano.
-FOUNDATION_EXPORT double SyncanoVersionNumber;
-
-//! Project version string for Syncano.
-FOUNDATION_EXPORT const unsigned char SyncanoVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Syncano/PublicHeader.h>
 
 @class SCAPIClient;
 /**
