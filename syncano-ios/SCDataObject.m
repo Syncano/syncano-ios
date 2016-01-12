bcfe555b8bba2b9ab10ada2fa2e1978a5c9e1031
Jan Lipmann <jan.lipmann@likomp.pl>
2016-01-12 08:25:18 +0100

    [LIB-429] Added expected_revision

--- syncano-ios/SCDataObject.m
+++ syncano-ios/SCDataObject.m
@@ -108,12 +108,16 @@
                 completion(error);
             }
         } else {
-            NSDictionary *params = [[SCParseManager sharedSCParseManager] JSONSerializedDictionaryFromDataObject:self error:&error];
+            NSMutableDictionary *params = [[[SCParseManager sharedSCParseManager] JSONSerializedDictionaryFromDataObject:self error:&error] mutableCopy];
+            
             if (error) {
                 if (completion) {
                     completion(error);
                 }
             } else {
+                if (self.objectId && self.revision) {
+                    [params setObject:self.revision forKey:@"expected_revision"];
+                }
                 [apiClient POSTWithPath:[self path] params:params  completion:^(NSURLSessionDataTask *task, id responseObject, NSError *error) {
                     if (completion && error) {
                         completion(error);
@@ -208,7 +212,7 @@
         return;
     }
     if ([[[self class] propertyKeys] containsObject:key]) {
-        NSDictionary *params = @{key:value};
+        NSDictionary *params = @{key:value , @"expected_revision" : self.revision};
         [apiClient PATCHWithPath:[self path] params:params completion:^(NSURLSessionDataTask *task, id responseObject, NSError *error) {
             completion(error);
         }];
