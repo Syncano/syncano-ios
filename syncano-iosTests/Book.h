c8e1dad4363411f5257f34181adc1f0e7e70074d
Jan Lipmann <jan.lipmann@likomp.pl>
2016-02-01 11:57:17 +0100

    [LIB-446] added missing properties to Book class

--- syncano-iosTests/Book.h
+++ syncano-iosTests/Book.h
@@ -8,8 +8,12 @@
 
 #import "SCDataObject.h"
 #import "Author.h"
+#import "SCFile.h"
 
 @interface Book : SCDataObject
 @property (nonatomic,copy) NSString *title;
 @property (nonatomic,copy) NSNumber *numOfPages;
+@property (nonatomic, copy) SCFile *cover;
+@property (nonatomic, copy) SCFile *content;
+@property (nonatomic,copy) Author *author;
 @end
