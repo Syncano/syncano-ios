fad698fd9cc61f930849f5fe35ffcf9f420aa6c6
Jan Lipmann <jan.lipmann@likomp.pl>
2016-03-15 13:33:04 +0100

    [LIB-163] SCDataObject+Increment code documentation

--- syncano-ios/SCDataObject+Increment.h
+++ syncano-ios/SCDataObject+Increment.h
@@ -9,7 +9,14 @@
 #import "SCDataObject.h"
 
 @interface SCDataObject (Increment)
-
+/**
+ *  Increment key's value by provided number
+ *
+ *  @param keys                  NSDictionary with key name as a key and increment by value as a value
+ *  @param apiClient             SCApiClinet to save data with
+ *  @param completion            competion block
+ *  @param revisionMismatchBlock result of revision mismatch verification block
+ */
 - (void)incrementKeys:(NSDictionary<NSString*,NSNumber*>*)keys usingAPIClient:(SCAPIClient *)apiClient withCompletion:(SCCompletionBlock)completion revisionMismatchValidationBlock:(SCDataObjectRevisionMismatchCompletionBlock)revisionMismatchBlock;
 
 @end
