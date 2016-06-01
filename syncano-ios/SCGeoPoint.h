469114dd1e5b2f5c0ae04fc5737d9627ec6bbec0
Jan Lipmann <jan.lipmann@likomp.pl>
2016-06-01 07:29:54 +0200

    [LIB-648] added custom initializers

--- syncano-ios/SCGeoPoint.h
+++ syncano-ios/SCGeoPoint.h
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Syncano. All rights reserved.
 //
 
+#import <CoreLocation/CoreLocation.h>
 #import <Foundation/Foundation.h>
 #import "Mantle/Mantle.h"
 #import "SCConstants.h"
@@ -22,5 +23,16 @@ NS_ASSUME_NONNULL_BEGIN
  Longitude of point in degrees. Valid range is from `-180.0` to `180.0`.
  */
 @property (nonatomic, assign) double longitude;
+
++ (instancetype)geoPointWithLocation:(CLLocation *)location;
+
++ (instancetype)geoPointWithLatitude:(double)latitude longitude:(double)longitude;
+
+- (instancetype)initWithLocation:(CLLocation *)location;
+
+- (instancetype)initWithLatitude:(double)latitude longitude:(double)longitude;
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionaryValue error:(NSError * _Nullable __autoreleasing *)error;
+
 @end
 NS_ASSUME_NONNULL_END
--- syncano-ios/SCGeoPoint.m
+++ syncano-ios/SCGeoPoint.m
@@ -7,8 +7,42 @@
 //
 
 #import "SCGeoPoint.h"
+#import "NSObject+SCParseHelper.h"
+
 
 @implementation SCGeoPoint
+
++ (instancetype)geoPointWithLocation:(CLLocation *)location {
+    return [self geoPointWithLatitude:location.coordinate.latitude
+                            longitude:location.coordinate.longitude];
+}
+
++ (instancetype)geoPointWithLatitude:(double)latitude longitude:(double)longitude {
+    SCGeoPoint *geopoint = [[self alloc] initWithLatitude:latitude longitude:longitude];
+    return geopoint;
+}
+
+- (instancetype)initWithLocation:(CLLocation *)location {
+    return [self initWithLatitude:location.coordinate.latitude longitude:location.coordinate.longitude];
+}
+
+- (instancetype)initWithLatitude:(double)latitude longitude:(double)longitude {
+    self = [super init];
+    if (self) {
+        self.latitude = latitude;
+        self.longitude = longitude;
+    }
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionaryValue error:(NSError * _Nullable __autoreleasing *)error {
+    self = [super init];
+    if (self) {
+        self.latitude = [[dictionaryValue[@"latitude"] sc_numberOrNil] doubleValue];
+        self.longitude = [[dictionaryValue[@"longitude"] sc_numberOrNil] doubleValue];
+    }
+    return self;
+}
 
 + (NSDictionary *)JSONKeyPathsByPropertyKey {
     return [NSDictionary mtl_identityPropertyMapWithModel:[self class]];
