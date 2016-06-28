dc828dc53218f84e1a11ab528dacbedb53d1dd47
Jan Lipmann <jan.lipmann@likomp.pl>
2016-06-28 23:00:17 +0200

    [LIB-664] Added sort direction parameter to SCPlease

--- syncano-ios/SCConstants.m
+++ syncano-ios/SCConstants.m
@@ -44,6 +44,7 @@ NSString *const SCPleaseParameterFields = @"fields";
 NSString *const SCPleaseParameterExcludedFields = @"excluded_fields";
 NSString *const SCPleaseParameterPageSize = @"page_size";
 NSString *const SCPleaseParameterOrderBy = @"order_by";
+NSString *const SCPleaseParameterOrderAscending = @"SCPleaseParameterOrderAscending";
 NSString *const SCPleaseParameterIncludeCount = @"include_count";
 NSString *const SCPleaseParameterCacheKey = @"cache_key";
 
