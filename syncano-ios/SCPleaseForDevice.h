5a578ee4e6a604c58f288f9c2564971dbd489029
Jan Lipmann <jan.lipmann@likomp.pl>
2016-07-18 11:37:50 +0200

    [LIB-811] Added SCPleaseForDevice class

---
+++ syncano-ios/SCPleaseForDevice.h
@@ -0,0 +1,13 @@
+//
+//  SCPleaseForDevice.h
+//  syncano-ios
+//
+//  Created by Jan Lipmann on 18/07/16.
+//  Copyright © 2016 Syncano. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCPleaseForDevice : NSObject
+
+@end
---
+++ syncano-ios/SCPleaseForDevice.m
@@ -0,0 +1,13 @@
+//
+//  SCPleaseForDevice.m
+//  syncano-ios
+//
+//  Created by Jan Lipmann on 18/07/16.
+//  Copyright © 2016 Syncano. All rights reserved.
+//
+
+#import "SCPleaseForDevice.h"
+
+@implementation SCPleaseForDevice
+
+@end
